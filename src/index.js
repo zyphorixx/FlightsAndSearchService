f75ad51d23d23aeeaf562fd2c3228a2e24dc3036
zyphorixx <24256@iiitu.ac.in>
2025-12-24 22:11:05 +0530

    Completed basic sequelize setup for creating a db

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const setupAndStartServer = async () => {
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended : true}));
-  
+
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });

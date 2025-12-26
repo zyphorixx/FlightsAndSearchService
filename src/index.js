a934752dbadf81ec55effd06e155c3b0be588621
zyphorixx <24256@iiitu.ac.in>
2025-12-26 19:23:05 +0530

    Completed city repository

--- src/index.js
+++ src/index.js
@@ -17,3 +17,4 @@ const setupAndStartServer = async () => {
 };
 
 setupAndStartServer();
+
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,13 +3,15 @@ const { City } = require('../models/city');
 class CityRepository {
     async createCity ({name}) { // destructuring the object then and there
         try {
-            const city = await City.create({name});
+            const city = await City.create({name}); // fetching the object
             return city;
-        } catch (error){
+        } 
+        catch (error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
-
+    
     async deleteCity (cityId) {
         try {
             await City.destroy({
@@ -17,8 +19,41 @@ class CityRepository {
                     id : cityId
                 }
             });
-        } catch(error){ // to catch database lvl errors
+        }
+        catch(error){ // to catch database lvl errors
+            console.log("Something went wrong in the repository layer");
             throw {error};
+        }
+    }
+
+    async updateCity (data , cityId) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id : cityId
+                }
+            });
+            return city;
+        }
+        catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+           const city = await City.findByPk(cityId);
+
+           // city object fetch krte hain findByPk() se
+           // Agar city mil jaati hai to city object return hoga
+           // Agar nahi milti to null return hoga
+
+           return city;
+        } 
+        catch (error) {
+           console.log("Something went wrong in the repository layer");
+           throw { error };
         }
     }
 }
